f0446c6a4cf30b1aa6528f28ae9dd10361e136d6
BFSmx5 <97541881+BSFmx5@users.noreply.github.com>
2022-03-22 14:18:20 -0600

    Add files via upload

---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+//Importa las librerias
+
+
+import TransactionScreen from "../screens/Transaction";
+import SearchScreen from "../screens/Search";
+// Crea tab
+
+export default class BottomTabNavigator extends Component {
+  render() {
+    return (
+      //Añade el código para añadir navigationcontainer
+
+      
+    );
+  }
+}
