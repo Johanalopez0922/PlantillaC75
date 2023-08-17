486ba825696528e7d30bd7f72934df84328d5d18
Johanalopez0922 <121140087+Johanalopez0922@users.noreply.github.com>
2023-08-17 12:16:03 -0500

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -2,8 +2,6 @@ import React, { Component } from "react";
 //Importa las librerias
 
 
-import TransactionScreen from "../screens/Transaction";
-import SearchScreen from "../screens/Search";
 // Crea tab
 
 export default class BottomTabNavigator extends Component {
